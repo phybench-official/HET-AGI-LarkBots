af2fab8ebdf849e28bd1579578728951c1878e3f
parkcai <162147799+parkcai@users.noreply.github.com>
2025-11-01 22:29:00 +0800

    [fix] debug: minor change

--- library/fundamental/my_lark.py
+++ library/fundamental/my_lark.py
@@ -12,7 +12,7 @@ def get_lark_app_token(
     app: str,
 )-> Tuple[str, str]:
     
-    app_token_dict = load_from_json("lark_api_key.json")
+    app_token_dict = load_from_json("lark_api_keys.json")
     app_id = app_token_dict[app]["app_id"]
     app_secret = app_token_dict[app]["app_secret"]
     return app_id, app_secret
