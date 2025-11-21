65212fb690f2b8f5ff8d976aec654b109a050669
wjsoj <wjs@wjsphy.top>
2025-11-21 11:58:26 +0800

    add webhook trigger

--- scripts/start_robots.py
+++ scripts/start_robots.py
@@ -3,15 +3,15 @@ from library import *
 
 def main():
     
-    problem_solver = ProblemSolverBot(
-        config_path = f"configs/problem_solver_config.yaml",
-    )
-    problem_solver.start()
+    # problem_solver = ProblemSolverBot(
+    #     config_path = f"configs/problem_solver_config.yaml",
+    # )
+    # problem_solver.start()
     
-    PKU_PHY_fermion_for_testing = PkuPhyFermionBot(
-        config_path = f"configs/pku_phy_fermion_config_for_testing.yaml",
-    )
-    PKU_PHY_fermion_for_testing.start()
+    # PKU_PHY_fermion_for_testing = PkuPhyFermionBot(
+    #     config_path = f"configs/pku_phy_fermion_config_for_testing.yaml",
+    # )
+    # PKU_PHY_fermion_for_testing.start()
     
     PKU_PHY_fermion = PkuPhyFermionBot(
         config_path = f"configs/pku_phy_fermion_config_251120_0900.yaml",
