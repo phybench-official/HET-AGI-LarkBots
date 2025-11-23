7d41513a76ff9a1a162dce08b4b673ead0e1ea9a
parkcai <162147799+parkcai@users.noreply.github.com>
2025-11-23 22:19:12 +0800

    fix: minor change

--- library/fundamental/lark_tools/lark_bot.py
+++ library/fundamental/lark_tools/lark_bot.py
@@ -40,8 +40,8 @@ class LarkBot:
     
     language_text_style = {
         "Plain Text": 1,
-        "Python": 9,
         "JSON": 28,
+        "Python": 49,
     }
     
     image_placeholder = f"<image_{never_used_string}>"
